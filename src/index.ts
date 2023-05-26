64eb6573535a43b6d55871068f4a9b7ea18bd434
jeffrey p thomas <6521767+rabiddroid@users.noreply.github.com>
2023-05-25 20:38:32 -0700

    refactor: added chained questions functionality

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import { OpenAI } from "langchain/llms/openai";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { RetrievalQAChain } from "langchain/chains";
+import { BaseChain, ConversationalRetrievalQAChain, RetrievalQAChain } from "langchain/chains";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { prompt } from './prompt.ts';
 
@@ -14,12 +14,12 @@ const MODEL_NAME = "text-davinci-003";// "gpt-3.5-turbo";
 
 export const run = async () => {
   // Initialize the LLM to use to answer the question.
-  const model = 
-  new OpenAI({
-    modelName: MODEL_NAME,
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    cache: true,
-  });
+  const model =
+    new OpenAI({
+      modelName: MODEL_NAME,
+      openAIApiKey: process.env.OPENAI_API_KEY,
+      cache: true,
+    });
 
 
   const loader = new PDFLoader("/Users/jeffrey.thomas/Development/personal/langchain-ts-demo/resources/Risk-Wiki.pdf", { splitPages: true });
@@ -34,18 +34,29 @@ export const run = async () => {
   const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
 
   // Create a chain that uses the OpenAI LLM and HNSWLib vector store.
-  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+  const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+
+  await answerQuestion(chain);
 
 
-  const question = await prompt('What is your question about the game - Risk?\n');
-  
-  const res = await chain.call({
-    query: question,
-  });
-
-  console.log({ res });
 
 };
 
+async function answerQuestion(chain: BaseChain, chatHistory: string[] = []) {
+
+  const question = await prompt('What is your question about the game - Risk?\n');
+  if (question.trim().toLowerCase() === 'quit') {
+    return;
+  }
+
+  const res = await chain.call({
+    question,
+    chat_history: [],
+  });
+  console.log({ res });
+
+  chatHistory.push(question + res.text);
+  await answerQuestion(chain, chatHistory);
+}
 
 await run();
